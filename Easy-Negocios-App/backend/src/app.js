f63a66be5d2c4a8d5935a0f204b9cabbc94cb528
Diego Hurtado <diegohurtado@ug.uchile.cl>
2022-04-12 16:02:35 -0400

    Basic queries working for projects

--- Easy-Negocios-App/backend/src/app.js
+++ Easy-Negocios-App/backend/src/app.js
@@ -3,7 +3,7 @@ import projectsRoutes from './routes/projects'
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); // express.json parses incoming requests with JSON and is a body-parser
 
 app.use(projectsRoutes)
 
--- Easy-Negocios-App/backend/src/controllers/projects.js
+++ Easy-Negocios-App/backend/src/controllers/projects.js
@@ -2,15 +2,23 @@ import {connect} from '../database'
 
 //console.log(req.params)
 
+/*
+getProjects: route -> /projects
+    GET all projects in the database
+*/
 export const getProjects = async (req, res) => {
     const connection = await connect();
-    const [rows] = await connection.query('SELECT * FROM projects')
-    res.json(rows)
+    const [results] = await connection.query('SELECT * FROM projects')
+    res.json(results)
 }
 
+/*
+getProject: route -> /projects/:id
+    GET an specific project
+*/
 export const getProject = async (req, res) => {
     const connection = await connect();
-    const [rows] = await connection.query("SELECT * FROM projects WHERE id = ?",
+    const [results] = await connection.query("SELECT * FROM projects WHERE id = ?",
     [
         req.params.id,
     ],
@@ -18,20 +26,36 @@ export const getProject = async (req, res) => {
         console.log(error)
         console.log(results)
     });
-    res.json(rows[0])
+    res.json(results[0])
 }
 
+/*
+createProject: route -> /projects
+    POST a new project to the database, requires a body with
+        project_name - NOT NULL
+        project_description - CAN BE NULL
+*/
 export const createProject = async (req, res) => {
     const connection = await connect();
-    const results = await connection.query(
+    const [results] = await connection.query(
         "INSERT INTO projects (project_name, project_description) VALUES (?, ?)",
         [
             req.body.project_name,
             req.body.project_description,
         ]);
-    console.log(results)
+    //console.log(results)
+    // This way of returning is in the web response, not console, so we are returning the insertId of the
+    //      new element and the data we sent
+    res.json({
+        id: results.insertId,
+        ...req.body,
+    });
 }
 
+/*
+deleteProjects: route -> /projects/:id
+    DELETE an specific project from the database
+*/
 export const deleteProject = async (req, res) => {
     const connection = await connect();
     const results = await connection.query(
@@ -40,9 +64,22 @@ export const deleteProject = async (req, res) => {
             req.params.id,
         ]
     )
-    console.log(results)
+    //console.log(results)
+    res.sendStatus(204);
 }
 
 export const updateProject = async (req, res) => {
-    res.send("Hello World!!")
+    const connection = await connect();
+    const [results] = await connection.query(
+        "UPDATE projects SET ? WHERE id = ?",
+        [
+            req.body,
+            req.params.id,
+        ]
+    )
+    //console.log([results])
+    res.json({
+        changes_made: results.info,
+        ...req.body,
+    });
 }
