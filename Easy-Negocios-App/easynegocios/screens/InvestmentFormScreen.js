8479d07ef726e7df7ea5640f9104d279d0f1827a
Diego Hurtado <diegohurtado@ug.uchile.cl>
2022-06-23 15:08:50 -0400

    DatePicker for InvestmentForm is ready and working perfectly, updating data in the database

--- Easy-Negocios-App/easynegocios/screens/InvestmentFormScreen.js
+++ Easy-Negocios-App/easynegocios/screens/InvestmentFormScreen.js
@@ -1,7 +1,8 @@
 import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native'
 import { useIsFocused } from '@react-navigation/native';
 import React, { useState, useEffect } from 'react';
-import { Picker } from '@react-native-picker/picker'
+import { Picker } from '@react-native-picker/picker';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 import Layout from '../components/Layout'
 import { saveInvestment, getInvestment, updateInvestment } from '../api'
@@ -18,7 +19,8 @@ const InvestmentFormScreen = ({ navigation, route }) => {
     ticket: null,
     investment_description: null,
     owned_product: null,
-    cuantity: null
+    cuantity: null,
+    investment_date: null
   });
 
   const handleChange = (key, value) => setInvestment({ ...investment, [key]: value });
@@ -37,7 +39,8 @@ const InvestmentFormScreen = ({ navigation, route }) => {
           ticket: object.ticket,
           investment_description: object.investment_description,
           owned_product: object.owned_product,
-          cuantity: object.cuantity.toString()
+          cuantity: object.cuantity.toString(),
+          investment_date: object.investment_date ? new Date(object.investment_date.slice(0, 19)) : null
         })
       })();
     }
@@ -52,6 +55,12 @@ const InvestmentFormScreen = ({ navigation, route }) => {
       investment.total_net_price = parseInt(investment.total_net_price)
       investment.total_gross_price = parseInt(investment.total_net_price / 1.19)
 
+      if (investment.investment_date) {
+        investment.investment_date = investment.investment_date.toISOString().slice(0, 19).replace('T', ' ')
+      } else { // In this case it can be null (valid) or undefined, if its undefined, we need it to be null.
+        investment.investment_date = null
+      }
+
       if (editing) {
         await updateInvestment(route.params.object_id, investment, navigation.getState().routes[1].params.project_id)
       } else {
@@ -60,13 +69,37 @@ const InvestmentFormScreen = ({ navigation, route }) => {
       navigation.navigate('InvestmentListScreen', { project_id: navigation.getState().routes[1].params.project_id })
 
     } catch (error) {
-
+      console.error(error);
     }
 
   }
 
+  // Date Time picker from here below
+
+  const [mode, setMode] = useState('date');
+  const [show, setShow] = useState(false);
+
+  const onChange = (event, selectedDate) => {
+    setShow(false);
+    selectedDate && handleChange('investment_date', selectedDate);
+  };
+
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode('date');
+  };
+
+  const showTimepicker = () => {
+    showMode('time');
+  };
+
   return (
     <Layout>
+
       <TextInput
         style={styles.input}
         placeholder='Monto (neto) de compra'
@@ -74,12 +107,14 @@ const InvestmentFormScreen = ({ navigation, route }) => {
         onChangeText={(text) => handleChange('total_net_price', text.replace(/[^0-9]/g, ''))}
         value={investment.total_net_price}
       />
+
       <TextInput
         style={styles.input}
         placeholder='Descripcion de la compra (opcional)'
         onChangeText={(text) => handleChange('investment_description', text)}
         value={investment.investment_description}
       />
+
       <Picker
         style={{ width: '90%' }}
         selectedValue={investment.ticket}
@@ -91,6 +126,7 @@ const InvestmentFormScreen = ({ navigation, route }) => {
         <Picker.Item label="Omitir" value={null} key={null} />
         <Picker.Item label="Boleta o Factura" value={null} key={null} />
       </Picker>
+
       <Picker
         style={{ width: '90%' }}
         selectedValue={investment.owned_product}
@@ -99,9 +135,10 @@ const InvestmentFormScreen = ({ navigation, route }) => {
         }>
         <Picker.Item label="Compra y Venta" value={1} key={1} />
         <Picker.Item label="No es compra y venta" value={0} key={0} />
-        <Picker.Item label="Omitir" value={0} key={null} />
-        <Picker.Item label="Tipo de Compra" value={0} key={null} />
+        <Picker.Item label="Omitir" value={0} key={2} />
+        <Picker.Item label="Tipo de Compra" value={0} key={3} />
       </Picker>
+
       <TextInput
         style={styles.input}
         placeholder='Cantidad'
@@ -109,12 +146,43 @@ const InvestmentFormScreen = ({ navigation, route }) => {
         onChangeText={(text) => handleChange('cuantity', text.replace(/[^0-9]/g, ''))}
         value={investment.cuantity}
       />
-      {/* TODO: Date Picker Missing, also re-order the fields so the FE is optimal */}
+
+      <View style={styles.itemContainer}>
+
+        <TouchableOpacity
+          styles={styles.button}
+          onPress={showDatepicker}>
+          <Text style={styles.buttonText}>Selecciona Fecha</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          styles={styles.button}
+          onPress={showTimepicker}>
+          <Text style={styles.buttonText}>Selecciona Hora</Text>
+        </TouchableOpacity>
+
+      </View>
+
+      <View>
+
+        <Text>Fecha: {investment.investment_date ? investment.investment_date.toLocaleString() : "No seleccionada"}</Text>
+        {show && (
+          <DateTimePicker
+            testID="dateTimePicker"
+            value={investment.investment_date ? investment.investment_date : new Date()}
+            mode={mode}
+            is24Hour={true}
+            onChange={onChange}
+          />
+        )}
+      </View>
+
       <TouchableOpacity
         style={styles.button}
         onPress={handleSubmit}>
         <Text style={styles.buttonText}>{editing ? "Actualizar Compra" : "Guardar Compra"}</Text>
       </TouchableOpacity>
+
     </Layout>
   )
 }
@@ -142,7 +210,15 @@ const styles = StyleSheet.create({
   buttonText: {
     color: '#000000',
     textAlign: 'center',
-  }
+  },
+  itemContainer: {
+    padding: 15,
+    marginVertical: 30,
+    borderRadius: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    width: '100%',
+  },
 })
 
 export default InvestmentFormScreen
