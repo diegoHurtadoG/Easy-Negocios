7e4269deaca7925f75c2fd2dc6d00e67e9b8805a
Diego Hurtado <diegohurtado@ug.uchile.cl>
2022-07-18 09:32:28 -0400

    Will update firebase and try to fix with the forum

--- Easy-Negocios-App/easynegocios/App.js
+++ Easy-Negocios-App/easynegocios/App.js
@@ -3,8 +3,6 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Navigation from './screens/Navigation'
 import { NavigationContainer } from '@react-navigation/native'
 import { AuthProvider } from './components/Hooks/useAuth'
-import { LogBox } from 'react-native'
-LogBox.ignoreLogs(['AsyncStorage has been extracted from react-native core and will be removed in a future release']);
 
 const Stack = createNativeStackNavigator()
 
