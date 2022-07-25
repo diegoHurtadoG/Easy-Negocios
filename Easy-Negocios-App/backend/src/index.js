7539ec22b3b4e0738b5f8e8413aec91fe4677239
Diego Hurtado <diegohurtado@ug.uchile.cl>
2022-07-25 16:48:51 -0400

    Working and deployable version of the backend :)

--- Easy-Negocios-App/backend/src/index.js
+++ Easy-Negocios-App/backend/src/index.js
@@ -1,6 +1,6 @@
 import app from './app'
 
-const PORT = parseInt(process.env.PORT) || 3000;
+const PORT = parseInt(process.env.PORT) || 8080;
 //const HOST = '0.0.0.0'; // For local docker, update to app.listen(PORT, HOST, ()...)
 
 app.listen(PORT, () => {
