ce8bb9c24fbcfe319d58bc0b9c9b3e175d216052
Diego Hurtado <diegohurtado@ug.uchile.cl>
2022-07-25 02:43:25 -0400

    CloudSQL working when running local, error with cloud run

--- Easy-Negocios-App/backend/src/config.js
+++ Easy-Negocios-App/backend/src/config.js
@@ -1,18 +1,20 @@
 import { config as dotenv } from "dotenv"
 dotenv();
 
-export const config = process.env.ENV === "PROD" ? 
+// If .env has PROD env, then the cloud run credentials work, if not, the local connection to cloudsql
+
+export const config = (process.env.ENV === "PROD" || true) ? 
 {
     user: process.env.DB_GOOGLESQL_USER,
     password: process.env.DB_GOOGLESQL_PASSWORD,
     database: process.env.DB_GOOGLESQL_DATABASE,
-    socketPath: process.env.DB_GOOGLESQL_SOCKETPATH,
+    socketPath: `/cloudsql/${process.env.DB_GOOGLESQL_INSTANCE_CONNECTION_NAME}`,
     multipleStatements: true
 } : 
 {
-    host: process.env.DB_LOCAL_HOST,
-    user: process.env.DB_LOCAL_USER,
-    password: process.env.DB_LOCAL_PASSWORD,
-    database: process.env.DB_LOCAL_DATABASE,
+    host: process.env.DB_LOCAL_GOOGLE_HOST,
+    user: process.env.DB_LOCAL_GOOGLE_USER,
+    password: process.env.DB_LOCAL_GOOGLE_PASSWORD,
+    database: process.env.DB_LOCAL_GOOGLE_DATABASE,
     multipleStatements: true
 }
--- Easy-Negocios-App/backend/src/index.js
+++ Easy-Negocios-App/backend/src/index.js
@@ -1,8 +1,8 @@
 import app from './app'
 
 const PORT = parseInt(process.env.PORT) || 3000;
-const HOST = '0.0.0.0';
+//const HOST = '0.0.0.0'; // For local docker, update to app.listen(PORT, HOST, ()...)
 
-app.listen(PORT, HOST, () => {
+app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
